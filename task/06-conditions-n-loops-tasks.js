81d17c07ad42724030b86024ac146b87d67cd263
Nadia-Kovaleva <Nadia.kv13@gmail.com>
2020-07-18 18:10:34 +0300

    some syntax changes

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -235,13 +235,12 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
     } else {
       interval = "[" + a + ", " + b + ")";
     }
+  } else if (isEndIncluded) {
+    interval = "(" + a + ", " + b + "]";
   } else {
-    if (isEndIncluded) {
-      interval = "(" + a + ", " + b + "]";
-    } else {
-      interval = "(" + a + ", " + b + ")";
-    }
+    interval = "(" + a + ", " + b + ")";
   }
+
   return interval;
 }
 
@@ -460,13 +459,12 @@ function timespanToHumanString(startDate, endDate) {
     }
   } else if (period > 2.981e10 && period <= 4.709e10) {
     res = "a year ago";
+  } else if (period % 3.154e10 > 1.577e10) {
+    res = Math.ceil(period / 3.154e10) + " years ago";
   } else {
-    if (period % 3.154e10 > 1.577e10) {
-      res = Math.ceil(period / 3.154e10) + " years ago";
-    } else {
-      res = Math.floor(period / 3.154e10) + " years ago";
-    }
+    res = Math.floor(period / 3.154e10) + " years ago";
   }
+
   return res;
 }
 
@@ -519,7 +517,7 @@ function getCommonDirectoryPath(pathes) {
   partsFrom1.shift();
   for (let i = 0; i < partsFrom1.length; i++) {
     pathes.forEach((path) => {
-      if (path.search(RegExp(`${partsFrom1[i]}\/`)) < 0) {
+      if (path.search(RegExp(`${partsFrom1[i]}/`)) < 0) {
         flag = false;
       }
       if (path.charAt(0) != "/") {
