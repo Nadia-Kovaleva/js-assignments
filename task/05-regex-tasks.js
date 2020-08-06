300a01112ef262616132f9995e7d074334f7e8a8
Nadia-Kovaleva <Nadia.kv13@gmail.com>
2020-08-06 15:28:50 +0300

    fix getRegexForSSN func

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -87,7 +87,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-  return /^(?!(000))\d{3}-(?!00)\d{2}-(?!0000)\d{4}$/;
+  return /^(?!(000|666|9))\d{3}-(?!00)\d{2}-(?!0000)\d{4}$/;
 }
 
 /**
